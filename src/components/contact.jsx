94981223a8105fe9cfc8d91485301fcff7b6036a
Tim <timconwaydev@outlook.com>
2024-09-22 16:06:20 -0400

    Fix Submit Button. Add FavIcon

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,16 +4,17 @@ Tim Conway
 301495941
 September 21st, 2024
  */
-
+import { useNavigate } from "react-router-dom";
 export default function Contact() {
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
+    navigate("/");
     event.preventDefault();
   };
   return (
     <div>
       <div className="content">
         <h1>Contact me</h1>
-
         <h3>
           Please contact me directly at{" "}
           <a href="mailto:timconwaydev@outlook.com">Tim@TimConway.Dev</a> or
@@ -23,30 +24,34 @@ export default function Contact() {
       <br />
       <br />
       <br />
-      <div className="form">
+      <div>
         <form onSubmit={handleSubmit}>
-          <div>
-            <label>First Name:</label>
-            <input type="text" />
+          <div className="form">
+            <div>
+              <label>First Name:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Last Name:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Contact Number:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Email:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Message:</label>
+              <textarea />
+            </div>
           </div>
-          <div>
-            <label>Last Name:</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>Contact Number:</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>Message:</label>
-            <textarea />
-          </div>
-          <div>
-            <button type="submit">Submit</button>
+          <div style={{ textAlign: "center" }}>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
